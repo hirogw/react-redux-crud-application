cb73b75e4074f9cc16dd830da8fabffb471e4fe0
小川裕之 <o_hiroyuki@mac.com>
2020-02-21 16:52:32 +0900

    create functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input id="bar" type="text" onChange={() => {console.log('I am clicked')}} />
-    </React.Fragment>
-  );
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input id="bar" type="text" onChange={() => {console.log('I am clicked')}} />
+//     </React.Fragment>
+//   );
   
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
+const Cat = () => {
+  return <div>Weow!</div>
+}
+
+
+
 
 export default App;
