5729501a85983cae02dfd8a548ed2769021551ba
小川裕之 <o_hiroyuki@mac.com>
2020-02-21 14:44:52 +0900

    change tag

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { render } from '@testing-library/react';
 
 function App() {
-  return <div>hello world!</div>;
+  return <h2>hello world!</h2>;
+
+  // return React.createElement(
+  //   'h1',
+  //   null,
+  //   'hoge'
+  // );
 }
 
 export default App;
